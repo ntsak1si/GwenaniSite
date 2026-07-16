ff59cd9b2fd3077446c6cc54dfa728f8aa3709a5
ntsak1si <ntsakisi1504@gmail.com>
2026-07-16 19:53:45 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-window.addEventListener("scroll", function(){
-
-const header = document.querySelector("header");
-
-if(window.scrollY > 50){
-header.style.background="#111";
-}else{
-header.style.background="#222";
-}
-
-});
-
 const enquiryForm = document.querySelector("#enquiry-form");
 
 if (enquiryForm) {
